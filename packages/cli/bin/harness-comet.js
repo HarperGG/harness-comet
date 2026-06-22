3d300df71ce52dc23868697436fb0771975a3660
gangG <44334279+HarperGG@users.noreply.github.com>
2026-06-22 13:13:51 +0800

    wire comet bootstrap before cli startup

--- packages/cli/bin/harness-comet.js
+++ packages/cli/bin/harness-comet.js
@@ -2,12 +2,13 @@
 import packageJson from "../package.json" with { type: "json" };
 import { runSkillCommand } from "../dist/commands/skill-entry.js";
 import { main } from "../dist/index.js";
+import { bootstrapComet } from "./comet-bootstrap.js";
 
 if (process.argv.length === 3 && ["--version", "-V"].includes(process.argv[2])) {
   process.stdout.write(`${packageJson.version}\n`);
 } else {
-  runSkillCommand(process.argv)
-    .then((handled) => (handled ? undefined : main(process.argv)))
+  bootstrapComet(process.argv)
+    .then((argv) => (argv ? runSkillCommand(argv).then((handled) => (handled ? undefined : main(argv))) : undefined))
     .catch((error) => {
       console.error(error instanceof Error ? error.message : String(error));
       process.exitCode = 10;
